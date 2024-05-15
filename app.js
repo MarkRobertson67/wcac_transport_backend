80487ec1d57d7e2196ddd1212aa26a2c68dc5405
Mark Robertson <markrobertson67@gmail.com>
2024-05-15 17:06:57 -0400

    Added dependancies and app.js - Server up and running

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+//Controllers
+
+
+// HEALTH CHECK ROUTE
+app.get("/", (req, res) => {
+    const htmlContent = `
+        <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+            <p style="text-align: center; font-size: 24px;">
+                Welcome to the We Care Adult Care <strong>Back-end</strong>
+            </p>
+        </div>
+    `;
+    
+    // Send the HTML content as the response
+    res.send(htmlContent);
+    // res.json({ message: "Welcome to the We Care Adult care **Back-end**" });
+}
+);
+
+// NOT FOUND ROUTE
+app.get("*", (req, res) => {
+    res.status(404).json({ message: "Page Not Found" });
+});
+
+
+module.exports = app;
